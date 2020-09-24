5a1f7b04d1a51cf42735a886e628f08b4778c93e
monilgudhka <monil.gudhka@crimsoni.com>
2020-09-24 22:28:06 +0530

    removing dependency with position in distance formula

--- src/main/java/edu/challenge/beat/service/HaversineDistance.java
+++ src/main/java/edu/challenge/beat/service/HaversineDistance.java
@@ -1,21 +1,14 @@
 package edu.challenge.beat.service;
 
-import edu.challenge.beat.model.Position;
-
 public class HaversineDistance {
 
     private static final int R = 6371; // Radius of the earth
 
-    public static double getDistance(Position source, Position destination) {
-        double lat1 = source.getLatitude();
-        double lon1 = source.getLongitude();
-        double lat2 = destination.getLatitude();
-        double lon2 = destination.getLongitude();
-
-        double latDistance = toRad(lat2 - lat1);
-        double lonDistance = toRad(lon2 - lon1);
+    public static double getDistance(double latitudeSource, double longitudeSource, double latitudeDestination, double longitudeDestination) {
+        double latDistance = toRad(latitudeDestination - latitudeSource);
+        double lonDistance = toRad(longitudeDestination - longitudeSource);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
-                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                Math.cos(toRad(latitudeSource)) * Math.cos(toRad(latitudeDestination)) *
                         Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return R * c;
@@ -24,7 +17,7 @@ public class HaversineDistance {
     private static double toRad(Double value) {
         return value * Math.PI / 180;
     }
-    
+
     private HaversineDistance() {
     }
 
