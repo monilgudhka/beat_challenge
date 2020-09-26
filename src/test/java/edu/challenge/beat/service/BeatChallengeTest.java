65178c058810ea27db7a0f3ffc048fa571ee0c92
monilgudhka <monil.gudhka@crimsoni.com>
2020-09-26 23:24:54 +0530

    adding beat challenge test

--- src/test/java/edu/challenge/beat/service/BeatChallengeTest.java
+++ src/test/java/edu/challenge/beat/service/BeatChallengeTest.java
@@ -1,28 +1,67 @@
 package edu.challenge.beat.service;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import edu.challenge.beat.model.Position;
+import edu.challenge.beat.model.Ride;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class BeatChallengeTest {
 
-    @Test
-    void runMethodShouldThrowIOExceptionTest_01 ( ) {
-        assertThrows( IOException.class, () -> {
-            BufferedReader reader = Files.newBufferedReader(Paths.get ( "/emptypath"));
-        });
+    private final Converter converter = mock(Converter.class);
+
+    private final PositionAggregator positionAggregator = mock(PositionAggregator.class);
+
+    private final FareCalculator fareCalculator = mock(FareCalculator.class);
+
+    private final BeatChallenge beatChallenge = new BeatChallenge(converter, positionAggregator, fareCalculator);
+
+    private final Path input = Paths.get("src/test/resources/input.csv");
+
+    @BeforeEach
+    void createInput() throws IOException {
+        Files.write(input, "position".getBytes());
+    }
+
+    @AfterEach
+    void deleteInput() throws IOException {
+        Files.deleteIfExists(input);
+    }
+
+
+    private final Path output = Paths.get("src/test/resources/output.csv");
+
+    @AfterEach
+    void deleteOutput() throws IOException {
+        Files.deleteIfExists(output);
     }
 
     @Test
-    void runMethodShouldThrowIOExceptionTest_02 ( ) {
-        assertThrows( IOException.class, () -> {
-            BufferedWriter writer = Files.newBufferedWriter(Paths.get ( "/emptypath" ), StandardOpenOption.CREATE);
-        });
+    void runMethodShouldProcess() throws IOException {
+        Ride ride = new Ride(1);
+        Position position = new Position(1, 37, 23, 1405594957);
+
+        when(converter.convert("position")).thenReturn(position);
+        when(positionAggregator.aggregate(position)).thenReturn(Optional.of(ride));
+        when(fareCalculator.calculate(ride)).thenReturn(new Double(10));
+        when(converter.convert(ride)).thenReturn("ride");
+
+        beatChallenge.run(input, output);
+
+        assertEquals(10, ride.getFare());
+        List<String> data = Files.readAllLines(output);
+        assertEquals(1, data.size());
+        assertEquals("ride", data.get(0));
     }
 }
