0fb40df2394d7e4866e08d3744ac5a197b19499d
Sushant Borse <bsushant@athenahealth.com>
2020-09-26 19:08:07 +0530

    read file names from properties
    read file names from properties

--- src/main/java/edu/challenge/beat/BeatApplication.java
+++ src/main/java/edu/challenge/beat/BeatApplication.java
@@ -4,22 +4,33 @@ import edu.challenge.beat.service.BeatChallenge;
 import edu.challenge.beat.service.Converter;
 import edu.challenge.beat.service.FareCalculator;
 import edu.challenge.beat.service.PositionAggregator;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 /**
  * Main class
  */
 public class BeatApplication {
 
+    final private Properties properties = new Properties();
+
     public static void main(String[] args) throws IOException {
-        Path inputFile = Paths.get("src/main/resources/paths.csv");
-        Path outputFile = Paths.get("src/main/resources/output.csv");
-        //System.out.println("start time= "+System.currentTimeMillis ());
         long st = System.currentTimeMillis ();
 
+        //fetch input and output file names
+        Properties properties = new Properties();
+        String appConfigPath = "config.properties";
+        InputStream inputStream = Thread.currentThread ().getContextClassLoader ().getResourceAsStream ( appConfigPath );
+        properties.load(inputStream);
+
+        //read the file paths
+        Path inputFile = Paths.get(properties.getProperty ( "inputFilePath" ));
+        Path outputFile = Paths.get(properties.getProperty ( "outputFilePath" ));
+
+        //main task logic
         Converter converter = new Converter();
         PositionAggregator aggregator = new PositionAggregator();
         FareCalculator fareCalculator = new FareCalculator();
@@ -27,9 +38,9 @@ public class BeatApplication {
         BeatChallenge challenge = new BeatChallenge(converter, aggregator, fareCalculator);
         challenge.run(inputFile, outputFile);
 
-        //System.out.println("end time= "+System.currentTimeMillis ());
+
         long et = System.currentTimeMillis ();
-        System.out.println("Time taken in milliseconds= "+ (et - st));
+        System.out.println("Time taken to run the task(in milliseconds)= "+ (et - st));
     }
 
 }
